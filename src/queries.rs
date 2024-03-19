a522eaffbb3fb6c4a8cabd770d21c456fda02c46
moliva <oliva.miguelh@gmail.com>
2024-03-19 12:17:42 -0300

    refactor: minor style changes and imports

--- src/queries.rs
+++ src/queries.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
 use sqlx::{postgres::PgPoolOptions, PgPool};
 use uuid::Uuid;
--- src/routes/status.rs
+++ src/routes/status.rs
@@ -3,5 +3,5 @@ use actix_web::{get, Error, HttpResponse, Result};
 #[get("/status")]
 pub async fn status() -> Result<HttpResponse, Error> {
     let status = "everything working!";
-    Ok(HttpResponse::Ok().json(&status))
+    Ok(HttpResponse::Ok().json(status))
 }
